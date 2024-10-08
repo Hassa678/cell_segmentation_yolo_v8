74de34e70575147390d7e306494b6acc5db7f932
unknown <you@example.com>
2024-10-08 01:04:07 +0100

    requirements

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'cellSegmentation_v8',
+    version= '0.0.0',
+    author= 'hassan taj',
+    author_email= 'hassantaj00@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
