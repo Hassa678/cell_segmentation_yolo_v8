542abaa126236319c3951429a9d6933f6adf7f18
unknown <you@example.com>
2024-10-09 23:57:32 +0100

    validation data

--- cellSegmentation_v8/pipeline/training_pipeline.py
+++ cellSegmentation_v8/pipeline/training_pipeline.py
@@ -2,7 +2,7 @@ import sys, os
 from cellSegmentation_v8.logger import logging
 from cellSegmentation_v8.exception import AppException
 from cellSegmentation_v8.components.data_ingestion import DataIngestion
-
+from cellSegmentation_v8.components.data_validation import DataValidation
 
 
 from cellSegmentation_v8.entity.config_entity import (DataIngestionConfig,
@@ -44,13 +44,37 @@ class TrainPipeline:
         except Exception as e:
             raise AppException(e, sys)
         
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
 
+        try:
+            data_validation = DataValidation(
+                data_ingestion_artifact=data_ingestion_artifact,
+                data_validation_config=self.data_validation_config,
+            )
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+
+            logging.info("Performed the data validation operation")
+
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
+
+            return data_validation_artifact
+
+        except Exception as e:
+            raise AppException(e, sys) from e
 
 
 
     def run_pipeline (self) -> None:
         try:
             data_ingestion_artifact = self.start_data_ingestion()
+            
+            data_validation_artifact = self.start_data_validation(
+                data_ingestion_artifact=data_ingestion_artifact
+            )
         except Exception as e:
             raise AppException(e, sys)
 
